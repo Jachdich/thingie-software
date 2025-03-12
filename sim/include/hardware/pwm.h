c65c990edcd10941a1351736ad12b6d988f0e5df
Jachdich <jamesedkitching@gmail.com>
2025-03-12 17:31:03 +0000

    Simulation

---
+++ sim/include/hardware/pwm.h
@@ -0,0 +1,3 @@
+#define PWM_CHAN_A 1
+#define GPIO_OUT 1
+#define GPIO_FUNC_PWM 1
---
+++ sim/include/pico/time.h
@@ -0,0 +1,5 @@
+#ifndef _PICO_TIME_H
+#define _PICO_TIME_H
+typedef struct {} repeating_timer_t;
+typedef struct {} alarm_pool_t;
+#endif
