993ee697902eaedfd961d679fd04e35fc8cbe8e5
Jachdich <jamesedkitching@gmail.com>
2026-01-21 21:48:15 +0000

    limit to 30fps

--- sim/src/multicore.c
+++ sim/src/multicore.c
@@ -1,4 +1,5 @@
 #include <pthread.h>
+#include "../include/pico/time.h"
 void *pthread_entry(void *arg) {
     void (*entry)(void) = arg;
     entry();
@@ -9,5 +10,14 @@ void multicore_launch_core1(void(*entry)(void)) {
      pthread_t thread1;
      pthread_create(&thread1, NULL, pthread_entry, entry);
  }
+
+ long o = 0;
  void multicore_fifo_pop_blocking() {}
- void multicore_fifo_push_blocking() {}
+ void multicore_fifo_push_blocking() {
+     long taken = to_us_since_boot(get_absolute_time()) - o;
+     long wait = (1000000 / 30) - taken;
+     if (wait > 0) {
+         sleep_us(wait - 5);
+     }
+     o = to_us_since_boot(get_absolute_time());
+ }
